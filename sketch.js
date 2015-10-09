b9534c4e67ef9ffef44769d8db209ae2082362fe
wendellaching <vincent23>
2015-10-09 16:34:33 -0400

    90% background
    ground done
    50% tree done
    still need help with triangles

--- sketch.js
+++ sketch.js
@@ -1,16 +1,41 @@
 function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
+	noStroke();
 }
 
 function draw() {
 	// clear the background
-	background(50, 50, 50);
+	if (mouseIsPressed) {
+		background(random(255, 0, 0));
+	} else {
+		background(0, 0, 50);
 
-	// set the drawing style
+	}
+	//grass ground
+	fill(0, 80, 50);
+	rect(0, 275, 640, 100);
+	//tree
+	fill(0, 40, 25);
+	rect(0, 250, 640, 50);
+	//triangle
+	// for (var x = 0; x < 10; x++) {
+	triangle(60, 100, 60, 20, 86, 75);
+	// }
+
+	//ufo
 	fill(100, 100, 100);
-	stroke(255, 255, 255);
+	ellipse(mouseX, mouseY, 40, 60);
+	ellipse(mouseX, mouseY + 20, 70, 40);
+	// circle details
+	fill(0, 0, 0);
+	ellipse(mouseX, mouseY + 20, 5, 5);
+	ellipse(mouseX - 20, mouseY + 20, 5, 5);
+	ellipse(mouseX - 34, mouseY + 20, 5, 5);
+	ellipse(mouseX + 20, mouseY + 20, 5, 5);
+	ellipse(mouseX + 34, mouseY + 20, 5, 5);
+	//background
 
-	// draw an ellipse
-	ellipse(320, 180, 100, 100);
+
+
 }
